6d34de4bee024698157d72f6c5fcb5bd283feb69
verybeautytang <353332175@qq.com>
2025-11-13 07:48:38 +0800

    feat: add picocolors and prompts plugins

--- packages/cli/src/commands/basic/info.ts
+++ packages/cli/src/commands/basic/info.ts
@@ -1,10 +1,12 @@
-//  实现日志
-// 实现颜色
-// 实现拉取模板
-
+//  实现日志插件
 import { logger } from '../../utils/logger'
+
+// 实现颜色插件
+import picocolors from 'picocolors'
 
 export const info = () => {
   // 打印日志，不用 console.log,自己实现
   logger.info('this is Info')
+  // 颜色配置
+  logger.info(picocolors.green(`How are ${picocolors.italic(`you`)} doing?`))
 }
